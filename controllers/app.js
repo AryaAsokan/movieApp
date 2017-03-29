0188b9bc277049e93c7b37c71c7db1898cf9a49b
AryaAsokan <ariyaasokan@gmail.com>
2017-03-29 18:47:46 +0530

    Added more details

--- controllers/app.js
+++ controllers/app.js
@@ -3,11 +3,15 @@ movieApp.controller('mainController', function($scope , $location, $http) {
 	$scope.message = 'Welcomeee';
 	this.movieName = {};
 	this.url = $location.absUrl();
-	$scope.search = "Sherlock Holmes";
+	//$scope.search = "Sherlock Holmes";
  	this.getMovie = function () {
  		$http.get("http://www.omdbapi.com/?t=" + $scope.search + "&tomatoes=true&plot=full").then(
  		function(response){
  		 $scope.details = response.data;
  		 }); 
+ 		 $http.get("http://www.omdbapi.com/?s=" + $scope.search).then(
+ 		 	function(response){ 
+ 		 		$scope.related = response.data; 
+ 		 	});
  	};
 });
